4e0fc0f075dafbff88e078f307ca61d268599643
Will Anderson <will@itsananderson.com>
2014-08-30 23:10:48 -0700

    Clean up client-side comment length code

--- short-comment-filter.js
+++ short-comment-filter.js
@@ -2,29 +2,25 @@
  * Filter comments that are too short
  */
 
-jQuery(document).ready(function($){
+jQuery(function($){
     $('#commentform').submit(function(e){
         var comment = $('#comment').val().replace(/\s+/g, ' ').replace(/^\s+|\s+$/g, '');
         var filterType = short_comment_settings.filter_type;
         var minCount = short_comment_settings.min_count;
-        var filterMessage = short_comment_settings.filter_message;
-        filterMessage = filterMessage.replace('%length%', minCount).replace('%type%', filterType);
-        if (filterType == 'words'){
-            var words = comment.split(' ');
-            if ( words.length < minCount ) {
-                alert(filterMessage);
-                e.preventDefault();
-                return false;
-            }
-            return true;
-        } else if ( filterType == 'characters' ) {
-            if ( comment.length < minCount ) {
-                alert(filterMessage);
-                e.preventDefault();
-                return false;
-            }
-            return true;
+        var filterMessage = short_comment_settings.filter_message
+                                .replace('%length%', minCount)
+                                .replace('%type%', filterType);
+
+        if ('words' === filterType && comment.split(' ') < minCount){
+            alert(filterMessage);
+            e.preventDefault();
+            return false;
+        } else if ('characters' === filterType && comment.length < minCount) {
+            alert(filterMessage);
+            e.preventDefault();
+            return false;
         }
+
         return true;
     });
 });
